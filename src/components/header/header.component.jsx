39a5fa3dc9d19bf0f6afee466b2e34f4f636626f
Roman Kolerov <coder8080@mail.ru>
2022-03-25 18:07:18 +0300

    Added adaptive header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,20 +1,40 @@
 import "./header.styles.scss";
+import { Component } from "react";
 
-const Header = () => (
-  <div className="header">
-    <div className="header-title">
-      <i className="fa-solid fa-gamepad"></i> warplay.cloud
-    </div>
-    <div className="options-container">
-      <span className="option active">Main</span>
-      <span className="option">Servers</span>
-      <span className="option">Download</span>
-      <span className="option">Open in browser</span>
-      <span className="option">Pricing</span>
-      <span className="option">Support</span>
-      <span className="option">Contacts</span>
-    </div>
-  </div>
-);
+class Header extends Component {
+  state = {
+    active: false,
+  };
+
+  toggleHidden = () => {
+    this.setState((state) => ({
+      active: !state.active,
+    }));
+  };
+
+  render() {
+    return (
+      <div className="header">
+        <div className="header-title">
+          <i className="fa-solid fa-gamepad"></i> warplay.cloud
+        </div>
+        <button onClick={() => this.toggleHidden()}>
+          <i className="fa-solid fa-bars"></i>
+        </button>
+        <div
+          className={`options-container ${this.state.active ? "active" : ""}`}
+        >
+          <span className="option active">Main</span>
+          <span className="option">Servers</span>
+          <span className="option">Download</span>
+          <span className="option">Open in browser</span>
+          <span className="option">Pricing</span>
+          <span className="option">Support</span>
+          <span className="option">Contacts</span>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Header;
